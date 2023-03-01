8a6a91877825f06efa4e3b014577da1d8d0edf7f
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-03-01 08:45:20 +0000

    Add viewing tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,16 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
+    let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
     // datepicker initialization
-    let datepicker = documenet.querySelectorAll('.datepicker');
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
         i18n: {done: "Select"}
     });
 
     // select initialization
-    let selects = document.querySelectorAll('select');
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // collapsible initialization
+    var collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
